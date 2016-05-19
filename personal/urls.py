740a0fce6bcf92dd18b51b00f809dc39afe59da7
Cody Wilson <Cheex11@gmail.com>
2016-05-19 10:57:13 -0700

    added index page

--- personal/urls.py
+++ personal/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'home/', views.home, name='home'),
     url(r'contact/', views.contact, name='contact'),
-    url(r'^$', views.coming_soon, name='coming_soon'),
+    url(r'^$', views.index, name='index'),
 ]
 	
--- personal/views.py
+++ personal/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
-def home(request):
-	return render(request, 'personal/home.html')
-
 def coming_soon(request):
 	return render(request, 'personal/coming_soon.html')
 
 def contact(request):
 	return render(request, 'personal/basic.html', {'content':['if you would like to contact me, please email me','cody@codyscapes.com']})
+
+def home(request):
+	return render(request, 'personal/home.html')
+
+def index(request):
+	return render(request, 'personal/index.html')
+
