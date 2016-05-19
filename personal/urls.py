f950312814aad4e13e5badbaec3c3212f89f867d
Cody Wilson <Cheex11@gmail.com>
2016-05-18 17:18:10 -0700

    added tutorial home page for learning practices.

--- personal/urls.py
+++ personal/urls.py
@@ -3,7 +3,7 @@ from . import views
 #from django.contrib import admin
 
 urlpatterns = [
-    #url(r'^$', views.index, name='index'),
+    url(r'home/$', views.home, name='home'),
     url(r'^$', views.coming_soon, name='coming_soon'),
 ]
 	
--- personal/views.py
+++ personal/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-def index(request):
+def home(request):
 	return render(request, 'personal/home.html')
 
 def coming_soon(request):
