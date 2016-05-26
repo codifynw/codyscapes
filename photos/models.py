2a2c2684b3f63d68abe4ad86ddefdac9c2cd45fe
Cody Wilson <Cheex11@gmail.com>
2016-05-25 23:43:24 -0700

    adding two fields to photos.

--- photos/models.py
+++ photos/models.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Photo(models.Model):
-    title = models.CharField(max_length=140) 
+    title = models.CharField(max_length=140)
+    location_string = models.CharField(max_length=50)
+    purchase_link = models.CharField(max_length=200)
     latitude = models.FloatField(default=32.7833333)
     longitude = models.FloatField(default=-96.7970)
     date = models.DateTimeField()
