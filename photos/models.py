da2aa8cbba23a1ce103e543421eec4ae2aa763e2
Cody Wilson <Cheex11@gmail.com>
2016-05-27 12:14:30 -0700

    changed defaults on photos model./

--- photos/models.py
+++ photos/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class Photo(models.Model):
     title = models.CharField(max_length=140)
-    location_string = models.CharField(max_length=50)
-    purchase_link = models.CharField(max_length=200)
+    location_string = models.CharField(max_length=50, default="The World")
+    purchase_link = models.CharField(max_length=200, default="http://www.lightontime.co")
     latitude = models.FloatField(default=32.7833333)
     longitude = models.FloatField(default=-96.7970)
     date = models.DateTimeField()
-    photo_file = models.FileField(upload_to='photos')
+    photo_file = models.FileField(upload_to='photos/upload/photos')
 
     def __str__(self):
         return self.title
