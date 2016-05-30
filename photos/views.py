9a20b6216a3a1a98decd6ed13a9c2eaaa3f8f8ee
Cody Wilson <Cheex11@gmail.com>
2016-05-29 19:24:15 -0700

    messing around with photos.  Does not work yet.

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Photo
 
 def index(request):
+	photos = Photo.objects.all()
 	return render(request, 'photos/index.html')
+
