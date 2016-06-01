b7ea8dafeae16dee79d473097c5cab65e727903b
Cody Wilson <Cheex11@gmail.com>
2016-06-01 07:34:23 -0700

    fixed URL pattern in photos to correctly find the media.

--- photos/urls.py
+++ photos/urls.py
@@ -7,6 +7,3 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
