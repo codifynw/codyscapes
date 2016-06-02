4160eae7d874b17faa886b38dba80ded227e4368
Cody Wilson <Cheex11@gmail.com>
2016-06-02 11:11:30 -0700

    migrated photos model, no longer saving media assets, updated index to use url instead of media asset.

--- photos/models.py
+++ photos/models.py
@@ -9,13 +9,14 @@ class Photo(models.Model):
     latitude = models.FloatField(default=32.7833333)
     longitude = models.FloatField(default=-96.7970)
     date = models.DateTimeField()
-    photo_file = models.ImageField(upload_to="photos/media/",
-    	null=False,
-    	blank=False,
-    	height_field="height_field",
-    	width_field="width_field")
-    height_field = models.IntegerField(default=0)
-    width_field = models.IntegerField(default=0)
+    #photo_file = models.ImageField(upload_to="photos/media/",
+    #	null=False,
+    #	blank=False,
+    #	height_field="height_field",
+    #	width_field="width_field")
+    #height_field = models.IntegerField(default=0)
+    #width_field = models.IntegerField(default=0)
+    url = models.CharField(max_length=200, default="http://www.codyscapes.com/gallery/")
 
     def __str__(self):
         return self.title
