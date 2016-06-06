0efecfd41d38c4e24fc9558e69dc447ce8ddde1a
Cody Wilson <Cheex11@gmail.com>
2016-06-05 20:49:49 -0700

    added a framework for a mobile slider.

--- photos/static/photos/js/main.js
+++ photos/static/photos/js/main.js
@@ -41,6 +41,30 @@ $( document ).ready(function() {
 	  cycleItems();
 	});
 
+	$(function() {
+	  $('#slides').slidesjs({
+	    width: 940,
+	    height: 528,
+	    callback: {
+	      loaded: function(number) {
+	        // Use your browser console to view log
+	        console.log('SlidesJS: Loaded with slide #' + number);
 
+	        // Show start slide in log
+	        $('#slidesjs-log .slidesjs-slide-number').text(number);
+	      },
+	      start: function(number) {
+	        // Use your browser console to view log
+	        console.log('SlidesJS: Start Animation on slide #' + number);
+	      },
+	      complete: function(number) {
+	        // Use your browser console to view log
+	        console.log('SlidesJS: Animation Complete. Current slide is #' + number);
 
+	        // Change slide number on animation complete
+	        $('#slidesjs-log .slidesjs-slide-number').text(number);
+	      }
+	    }
+	  });
+	});
 });
