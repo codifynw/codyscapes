2df98f9ee77f5cb49aa5d92ebcd6e34b1cea437a
Cody Wilson <Cheex11@gmail.com>
2016-06-11 21:45:14 -0700

    fixed annoying bug with mobile display caused by position being set to absolute for google maps id.

--- photos/views.py
+++ photos/views.py
@@ -7,6 +7,11 @@ def index(request):
 	photos = Photo.objects.all().filter(visible="true").order_by('-date')
 	return render(request, 'photos/index.html', {'photos': photos})
 
+
+def index2(request):
+	photos = Photo.objects.all().filter(visible="true").order_by('-date')
+	return render(request, 'photos/index2.html', {'photos': photos})
+
 #	def index(request):
 #		photos = Photo.objects.all().order_by('-date').filter(date__year='2016')
 #		return render(request, 'photos/index.html', {'photos': photos})
