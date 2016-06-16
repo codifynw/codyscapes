3dcab1e11c0c564095ab0c25eedfbea5f4f03181
Cody Wilson <Cheex11@gmail.com>
2016-06-15 18:34:29 -0700

    first form working

--- photos/urls.py
+++ photos/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 #from django.contrib import admin
 
 urlpatterns = [
-	url(r'/dev_form', views.dev_form, name='dev_form'),
+	url(r'dev_form', views.dev_form, name='dev_form'),
+	url(r'suggest$', views.suggestion_view, name='suggest'),
 	url(r'v1/photos(?P<pk>\d+)$',
 		views.RetrieveUpdateDestroyPhoto.as_view(),
 		name='photo_detail'),
--- photos/views.py
+++ photos/views.py
@@ -6,13 +6,19 @@ from rest_framework import generics
 from .models import Photo
 from . import models
 from . import serializers
+from . import forms
 
 def index(request):
 	photos = Photo.objects.all().filter(visible="true").order_by('-date')
 	return render(request, 'photos/index.html', {'photos': photos, 'key': os.environ['MAPS_KEY']})
 
 def dev_form(request):
-	return render(request, 'photos/index2.html')
+	form = forms.SuggestionForm()
+	return render(request, 'photos/index2.html', {'form': form})
+
+def suggestion_view(request):
+	form = forms.SuggestionForm()
+	return render(request, 'photos/suggestion_form.html', {'form': form})
 
 #API views
 class ListCreatePhoto(generics.ListCreateAPIView):
