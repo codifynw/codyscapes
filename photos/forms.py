d79b44b75094d287933115985677bf56821b7ede
Cody Wilson <Cheex11@gmail.com>
2016-06-15 20:02:30 -0700

    updated form and pip freeze.

--- photos/forms.py
+++ photos/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class SuggestionForm(forms.Form):
-	year = forms.IntegerField()
+	#year = forms.IntegerField()
+	year = forms.ChoiceField(choices=[(x, x) for x in range(2011, 2017)],label="")
 	#email = forms.EmailField()
 	#suggestion = forms.CharField(widget=forms.Textarea)
