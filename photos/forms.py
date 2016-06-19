749fd7d98cd44f48560c7743b8ed39fc6f738789
Cody Wilson <Cheex11@gmail.com>
2016-06-19 12:03:47 -0700

    made text on select buton larger on mobile display.

--- photos/forms.py
+++ photos/forms.py
@@ -4,7 +4,7 @@ class SuggestionForm(forms.Form):
 	#year = forms.IntegerField()
 	#year = forms.ChoiceField(choices=[(x, x) for x in range(2011, 2017)],
 	#							label="")
-	year = forms.ChoiceField(choices=[("all","Showing All Years"),("2016","2016"),("2015","2015")
+	year = forms.ChoiceField(choices=[("all","Choose a Year (Showing All)"),("2016","2016"),("2015","2015")
 										,("2014","2014"),("2013","2013"),("2012","2012"),("2011","2011")],
 								label="",
 								initial="all")
