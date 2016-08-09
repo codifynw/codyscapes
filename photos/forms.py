952276c2b1802cf38e52eb7f6721a5aed63244b1
Cody Wilson <Cheex11@gmail.com>
2016-08-09 15:01:53 -0500

    updated form to show new default value.

--- photos/forms.py
+++ photos/forms.py
@@ -4,9 +4,9 @@ class SuggestionForm(forms.Form):
 	#year = forms.IntegerField()
 	#year = forms.ChoiceField(choices=[(x, x) for x in range(2011, 2017)],
 	#							label="")
-	year = forms.ChoiceField(choices=[("all","Choose a Year (Showing All)"),("2016","2016"),("2015","2015")
+	year = forms.ChoiceField(choices=[("all","Show All Years"),("2016","2016"),("2015","2015")
 										,("2014","2014"),("2013","2013"),("2012","2012"),("2011","2011")],
 								label="",
-								initial="all")
+								initial="2016")
 	#email = forms.EmailField()
 	#suggestion = forms.CharField(widget=forms.Textarea)
