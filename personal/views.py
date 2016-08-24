a8f92f183e75b92b963d50ba8eef9829076fba2e
Cody Wilson <Cheex11@gmail.com>
2016-08-23 19:42:33 -0700

    favicon fix attempt

--- personal/views.py
+++ personal/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 
-def coming_soon(request):
-	return render(request, 'personal/coming_soon.html')
+# def coming_soon(request):
+# 	return render(request, 'personal/coming_soon.html')
 
-def contact(request):
-	return render(request, 'personal/basic.html', {'content':['if you would like to contact me, please email me','cody@codyscapes.com']})
+# def contact(request):
+# 	return render(request, 'personal/basic.html', {'content':['if you would like to contact me, please email me','cody@codyscapes.com']})
 
 def about(request):
 	return render(request, 'personal/about.html')
 
-def home(request):
-	return render(request, 'personal/home.html')
+# def home(request):
+# 	return render(request, 'personal/home.html')
 
 def index(request):
 	return render(request, 'personal/index.html')
 
-def license(request):
-	return render(request, 'personal/license.html')
+# def license(request):
+# 	return render(request, 'personal/license.html')
